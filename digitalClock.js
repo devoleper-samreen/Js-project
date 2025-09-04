1b6efba22f02f3f1c425b3443c79a3d3f0ef0f00
Samreen Malik <160000739+devoleper-samreen@users.noreply.github.com>
2025-09-04 14:50:03 +0530

    Update digitalClock.js

--- digitalClock.js
+++ digitalClock.js
@@ -4,9 +4,9 @@ setInterval(() => {
     let minute = time.getMinutes();
     let second = time.getSeconds();
 
-    hour = hour < 10 ? '0' + hour : hour;
-    minute = minute < 10 ? '0' + minute : minute;
-    second = second < 10 ? '0' + second : second;
+    // hour = hour < 10 ? '0' + hour : hour;
+    // minute = minute < 10 ? '0' + minute : minute;
+    // second = second < 10 ? '0' + second : second;
 
     console.log(`${hour} : ${minute} : ${second}`)
 }, 1000)
